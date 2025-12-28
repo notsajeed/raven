c7a130d529dc9a6804ed25729bf39d69d89c2d1c
notsajeed <sajeed725333@gmail.com>
2025-12-28 12:22:20 +0530

    add fly(push)

--- raven/cli.py
+++ raven/cli.py
@@ -2,6 +2,7 @@ import click
 from raven.commands.watch import watch
 from raven.commands.gather import gather
 from raven.commands.mark import mark
+from raven.commands.fly import fly
 
 @click.group()
 def cli():
@@ -11,6 +12,7 @@ def cli():
 cli.add_command(watch)
 cli.add_command(gather)
 cli.add_command(mark)
+cli.add_command(fly)
 
 if __name__ == "__main__":
     cli()
